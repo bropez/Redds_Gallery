06bf552d3bdb151b50d9c62d491b8439ae4eed12
Matthew <matthewbropez@gmail.com>
2018-07-15 02:46:14 -0400

    Redid reader.py
    It's 3 am and this bug was freaking me out. Turns out it was an ad on the table that ruined everything. I can finally sleep. I'll turn this output into a .csv file tomorrow so I can start loading stuff. Send help at church tomorrow.

--- reader.py
+++ reader.py
@@ -1,23 +1,45 @@
 import csv
 
-# the output is pic/query name/fake desc
 data_reader = csv.reader(open('out.csv'))
-for row in data_reader:
-    # this is eliminating the empty sets
-    if not row:
-        # print("List is empty")
-        pass
-    # this is eliminating the new lines
-    elif "\n" in row:
-        pass
-    # this is printing the art's name
-    elif "<br/>" in row:
-        print(row[4][1:-1])
-    # this works with the links for the pictures.
-    elif "<img src=" in row[0]:
-        print("https://www.thonky.com/animal-crossing-new-leaf/" + row[0][10:-3])
-    # this prints the first item in the set. It's usually the information we need.
+no_empties = []
+# this is stripping the empty sets and new lines I created
+for set in data_reader:
+    if set != [] and '\n' not in set:
+        no_empties.append(set)
+
+# this deletes the ad that I didn't know about but caused havoc
+for i, set in enumerate(no_empties):
+    if "<script" in set[0]:
+        del(no_empties[i])
+
+
+# this strips everything and formats them into strings for whatever reason
+for i, set in enumerate(no_empties):
+    if len(set) is 5:
+         no_empties[i] = set[4]
+    elif ".jpg" in set[0]:
+        no_empties[i] = "https://www.thonky.com/animal-crossing-new-leaf/" + set[0][10:-3]
     else:
-        print(row[0])
-        print('\n')
-    # print(row)
+        no_empties[i] = set[0]
+
+
+def chunk_it(l, n):
+    """
+    A method to chunk out the current list into smaller sublists
+    Thanks to Chris Albon for the code
+    https://chrisalbon.com/python/data_wrangling/break_list_into_chunks_of_equal_size/
+    :param l: the list to chunk from
+    :param n: the number of items you'd like in each chunk
+    :return: no return, just yield the chunks
+    """
+    for i in range(0, len(l), n):
+        yield l[i:i+n]
+
+
+chunks = list(chunk_it(no_empties, 3))
+
+for chunk in chunks:
+    print(chunk)
+# print(no_empties)
+# for thing in no_empties:
+#     print(thing)
